636bb164dca2e73c04d6bae7f6bcee3525f9c7ab
Peter Ngo-Ly <ngoly@sheridancollege.ca>
2019-11-12 15:12:17 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+d3.select('h1')
+    .style('color', 'red')
+    .attr('class', 'heading')
+    .text('Updated h1 tag');
+
+d3.select('body')
+.append('p')
+.text('first paragraph')
+
+d3.select('body')
+.append('p')
+.text('second paragraph')
+
+d3.select('body')
+.append('p')
+.text('third paragraph')
+
