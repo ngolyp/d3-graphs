8c7a8fbb4a5ac882cec4d903a87bb59a20eb3548
Peter Ngo-Ly <ngoly@sheridancollege.ca>
2019-11-12 16:06:20 -0500

    add bar graph

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-d3.select('h1')
-    .style('color', 'red')
-    .attr('class', 'heading')
-    .text('Updated h1 tag');
+var svgWidth = 500, svgHeight = 500, barPadding= 5;
+var barWidth = svgWidth / dataset.length;
 
-d3.select('body')
-.append('p')
-.text('first paragraph')
+var svg = d3.select('svg')
+            .attr('width', svgWidth)
+            .attr('height', svgHeight)
 
-d3.select('body')
-.append('p')
-.text('second paragraph')
+var xScale = d3.scaleLinear()
+                .domain([0, d3.max(dataset)])
+                .range([0, svgWidth]);
 
-d3.select('body')
-.append('p')
-.text('third paragraph')
+var yScale = d3.scaleLinear()
+            .domain([0, d3.max(dataset)])
+            .range([svgHeight, 0]);
 
+var x_axis = d3.axisBottom().scale(xScale);
+
+var y_axis = d3.axisLeft().scale(yScale);
+
+svg.append('g')
+    .attr('transform', 'translate(50, 10)')
+    .call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+svg.append('g')
+    .attr('transform', 'translate(50, ' + xAxisTranslate + ')')
+    .call(x_axis);
+
+var barChart = svg.selectAll('rect')
+                    .data(dataset)
+                    .enter()
+                    .append('rect')
+                    .attr('y', d => svgHeight-yScale(d))
+                    .attr('height',d => yScale(d))
+                    .attr('width', barWidth - barPadding)
+                    .attr('transform', function(d,i) {
+                    let translate = [barWidth * i, 0];
+                    return 'translate('+ translate +')';
+                    });
+
+var text = svg.selectAll('text')
+                .data(dataset)
+                .enter()
+                .append('text')
+                .text(d => d)
+                .attr('y', (d,i) => svgHeight - d -2)
+                .attr('x', (d,i) => barWidth * i);
